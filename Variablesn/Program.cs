d9ead9efc699dce13b4732ee74ec226f9aa69277
kumar6646 <sai.penusoft@gmail.com>
2025-06-24 10:50:16 +0530

    Add project files.

---
+++ Variablesn/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Variablesn
+{
+  
+    internal class Program
+    {
+       
+        /*
+         * Varibles: varible is used to store some value 
+         * it depends on type of the data.
+         * A varibale name should not start with a number and a special character
+         * except underscore(_)
+         * syntax:
+         * datatype <varibalename> =value;
+         * int a=100;
+         */
+        static void Main(string[] args)
+        {
+
+            //interger data types.
+            //int a = 100;
+            //place holder used to hold the value of a 
+            //Console.WriteLine("value of a is {0}",a);
+            //int AGE = 20;
+            //Console.WriteLine("your age is "+AGE);
+            int a, b, c;
+            a = 100;
+            b = 200;
+            c = a + b;
+          //  if a varible is with in a method it is a local varibles.
+            Console.WriteLine("sum of {0} and {1} is {2}",a,b,c);
+
+
+
+
+
+        }
+
+    }
+}
---
+++ Variablesn/Properties/AssemblyInfo.cs
@@ -0,0 +1,33 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("Variablesn")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("HP")]
+[assembly: AssemblyProduct("Variablesn")]
+[assembly: AssemblyCopyright("Copyright © HP 2025")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("1174f00c-7ffc-4a16-be05-59fe0988ad56")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version
+//      Build Number
+//      Revision
+//
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
